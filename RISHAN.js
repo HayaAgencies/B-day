56040672a42eb6b0d47660ea3f78535a5388aba1
HayaAgencies <riyashasanath@gmail.com>
2023-11-03 17:55:19 +0530

    szdf

---
+++ RISHAN.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    hi()
+})
+let page = 0
+function hi(){
+    
+}
+const play_anim = () =>{
+    const progress = document.querySelector(".Bookpage")
+    progress.setAttribute('id','play-anim')
+}
